364c6ba30a91793c7ff9ef68f905a6b0270b1410
webioann <webioann@gmail.com>
2026-01-10 11:06:18 +0200

    just testing and modifying

--- src/controllers/deleteUserById.ts
+++ src/controllers/deleteUserById.ts
@@ -2,7 +2,6 @@ import User from '../models/user.model.ts';
 import type { UserType } from '../types/user.types.ts';
 
 export const deleteUserById = async (id: string) => {
-    
     try {
         await User.findByIdAndDelete(id);
         console.log("User deleted successfully");
