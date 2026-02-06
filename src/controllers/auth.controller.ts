8f1464356007f7639439504b0fea15e628156bac
webioann <webioann@gmail.com>
2026-02-06 15:38:11 +0200

    set cookie options inside separated object and used

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,16 @@
-import type{ Request, Response } from 'express';
+import type{ Request, Response, CookieOptions } from 'express';
 import User from '../models/user.model.ts';
 import Token from '../models/token.model.ts';
 import bcrypt from 'bcryptjs';
 import config  from '../config/env.ts';
 import type { UserType } from '@shared-types/user.types.ts';
 import jwt from 'jsonwebtoken';
+
+const cookieOptions: CookieOptions = {
+    httpOnly: true,
+    secure: config.NODE_ENV === "production",
+    sameSite: "strict"
+}
 
 export const register =  async (req: Request, res: Response): Promise<void> => {
     try{
@@ -61,11 +67,7 @@ export const login =  async (req: Request, res: Response): Promise<void> => {
                 }
                 const accessToken = jwt.sign(payload, config.JWT_ACCESS_SECRET, { expiresIn: "1h" })
                 const refreshToken = jwt.sign(payload, config.JWT_ACCESS_SECRET, { expiresIn: "1h" })
-                res.cookie("refreshToken", refreshToken, {
-                    httpOnly: true,
-                    secure: config.NODE_ENV === "production",
-                    sameSite: "strict"
-                })
+                res.cookie("refreshToken", refreshToken, cookieOptions)
                 res.status(200).json({ 
                     token: accessToken,
                     message: "Login successful" ,
@@ -92,10 +94,15 @@ export const login =  async (req: Request, res: Response): Promise<void> => {
 
 export const logout = async (req: Request, res: Response): Promise<void> => {
     try{
-        res.status(204)
+        const refreshToken = req.cookies.refreshToken as string
+        if( refreshToken ) {
+            await Token.deleteOne({ token: refreshToken })
+        }
+        res.clearCookie("refreshToken", cookieOptions)
+        res.status(204).json({message: "Logout is successful"})
     } catch (error) {
         res
             .status(500)
-            .json({ message: `Something went wrong in time Logout` });
+            .json({ message: "Something went wrong in time Logout"});
     } 
 };
